3b5dc79128695ae306b775440220fdb9e0b338a4
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-16 10:51:30 +0200

    Added a character abstract class

--- src/com/dungeon_crawler/characters/Character.java
+++ src/com/dungeon_crawler/characters/Character.java
@@ -1,4 +1,29 @@
 package com.dungeon_crawler.characters;
 
-public class Character {
+public abstract class Character {
+    protected String name;
+    protected int xp;
+
+    public Character(String name, int xp) {
+        this.setName(name);
+        this.setXp(xp);
+    }
+
+    public abstract void getInfo();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
 }
