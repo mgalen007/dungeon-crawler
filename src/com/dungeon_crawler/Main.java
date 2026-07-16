322198222747ca3f5926958aa7600efc20bb87b7
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-16 10:56:52 +0200

    Brief main functions description

--- src/com/dungeon_crawler/Main.java
+++ src/com/dungeon_crawler/Main.java
@@ -1,5 +1,9 @@
 package com.dungeon_crawler;
 
+// To contain the game's main loop
+// Initializes everything (monsters, objects) and assigns to each room randomly
+// Handles user input
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
