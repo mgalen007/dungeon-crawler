f1b5282c962c11b4bcf8b8d96c4618173e5ea9b5
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-07-17 12:58:14 +0200

    Fixed missing goblin XP info, and potion availability info

--- src/com/dungeon_crawler/items/Potion.java
+++ src/com/dungeon_crawler/items/Potion.java
@@ -9,6 +9,6 @@ public class Potion extends Item {
     public void getInfo() {
         System.out.println("Item name: Potion");
         System.out.println("Value: " + this.getXpValue() + "XP");
-        System.out.println("Available: " + this.isUsed());
+        System.out.println("Available: " + !this.isUsed());
     }
 }
